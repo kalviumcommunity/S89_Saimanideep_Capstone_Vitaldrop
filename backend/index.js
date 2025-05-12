8fec0cdeb718fa7f0cf634dd791d36d9af9c6a08
saimanideep <saimanideep765@gmail.com>
2025-05-12 17:47:58 +0530

    adding changes

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
-
 // Routes
 app.get('/', (req, res) => {
   res.send('Vitaldrop Backend Running');
