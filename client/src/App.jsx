9aa9a8aaac2cb70d5c8f882846e81778510a7efb
saimanideep <saimanideep765@gmail.com>
2025-06-20 16:39:56 +0530

    react init

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div>
+      <h1>This is saimanideep's capstone project vitalDrop 🩸</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
